3ac9260211560ef4bfe0ee818306d0e29dd281fa
noway <dragonfury1928@gmail.com>
2024-03-28 18:08:39 -0400

    mysql server setup

--- database.js
+++ database.js
@@ -1,8 +1,29 @@
 import mysql from 'mysql2';
 
-mysql.createPool({
-	host: '127.0.0.1',
-	user: 'root',
-	password: '',
-	database: 'test'
+import 'dotenv/config';
+console.log(process.env)
+
+const pool = mysql.createPool({
+	host: process.env.MYSQL_HOST,
+	user: process.env.MYSQL_USER,
+	password: process.env.MYSQL_PASSWORD,
+	database: process.env.MYSQL_DATABASE,
 }).promise();
+
+export async function getNotes() {
+	const [rows] = await pool.query('SELECT * FROM notes');
+	return rows;
+}
+
+export async function getNote(id) {
+	const [rows] = await pool.query('SELECT * FROM notes WHERE id = ?', [id]);
+	return rows[0];
+}
+
+export async function createNote( title, content ) {
+	const [result] = await pool.query('INSERT INTO notes (title, content) VALUES (?, ?)', [title, content]);
+	return result.insertId;
+}
+
+const result = await createNote('Hello', 'World');
+console.log(result);
--- schema.sql
+++ schema.sql
@@ -4,11 +4,11 @@ USE notes_app;
 CREATE TABLE notes (
   id integer PRIMARY KEY AUTO_INCREMENT,
   title VARCHAR(255) NOT NULL,
-  contents TEXT NOT NULL,
+  content TEXT NOT NULL,
   created TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
-INSERT INTO notes (title, contents)
+INSERT INTO notes (title, content)
 VALUES
 ('My First Note', 'A note about something'),
 ('My Second Note', 'A note about something else');
