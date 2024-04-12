05c08a8fbfd48aa613d892dab8a00aa1a914b7b2
Kevin <vukevin0212@gmail.com>
2024-04-12 04:31:12 -0400

    added logo to navbar

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -2,13 +2,13 @@ import React from "react";
 import styles from "./Navbar.module.css";
 import { useNavigate } from "react-router-dom";
 import Button from "../Button/Button";
-
+import logo from "../../assets/logo.png";
 export default function Navbar() {
    const navigate = useNavigate();
 
    return (
       <div className={styles.navbar}>
-         <div className={styles.logo}>Logo</div>
+         <img src={logo} className={styles.logo} />
          <h1 className={styles.appName}>Infinite RSO</h1>
          <div className={styles.navControls}>
             <Button>Admin Toggle</Button>
