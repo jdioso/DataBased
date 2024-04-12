ea05df7977212eaab31ddac5c06f5f5f948cc748
Daisy Nguyen <93001657+dhashira@users.noreply.github.com>
2024-04-12 09:54:32 -0400

    Changing something small.

--- frontend/src/components/Form/Form.js
+++ frontend/src/components/Form/Form.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import "../styles/Login.css"; 
+import "./Form.css"; 
 
-function Login()
+function Form()
 {
     return(
         <div class='wrapper-login d-flex align-items-center justify-content-center'>
