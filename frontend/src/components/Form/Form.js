d8e737bc87bd1bc1c90323a4e3c44c5c2fabfeef
Daisy Nguyen <93001657+dhashira@users.noreply.github.com>
2024-04-13 00:29:23 -0400

    Fixed up the forms more.

--- frontend/src/components/Form/Form.js
+++ frontend/src/components/Form/Form.js
@@ -1,20 +1,11 @@
-import React from 'react';
-import "./Form.css"; 
+import React from "react";
+import styles from "./Form.module.css";
 
-function Form()
-{
-    return(
-        <div class='wrapper-login d-flex align-items-center justify-content-center'>
-            <div class='container align-items-center justify-content-center' id="loginDiv" data-aos="fade">
-                <form>
-                    <center><span id= "title">LOGIN</span></center>
-                    <center><input type="text" id="formInput" placeholder="USERNAME"/><br /></center>
-                    <center><input type="password" id="formInput" placeholder="PASSWORD"/><br /></center>
-                    <center><input type="submit" id="formButton" class="buttons" value="LOGIN"/></center>
-                </form>
-            </div>
-        </div>
-    );
-};
-
-export default Form;
+export default function Form({ formTitle, children, ...other }) {
+   return (
+      <div className={styles.form}>
+         <h2 className={styles.formTitle}>{formTitle}</h2>
+         {children}
+      </div>
+   );
+}
--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -1,11 +1,91 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
+import Button from "../../components/Button/Button"; 
+import Form from "../../components/Form/Form";
+import styles from "./Login.module.css";
 
-export default function Event() {
+export default function Login() {
    return (
       <>
-         <Navbar></Navbar>
-         <h1 className="heading">Login</h1>;
+         <Navbar />
+         <div className={styles.container}>
+            <div className={styles.flexRow}>
+               <Form formTitle="Login">
+                  <h2 className={styles.formDescriptor}>Username</h2>
+                  <center><input className={styles.formInput} type="text" /><br /></center>
+                  <h2 className={styles.formDescriptor}>Password</h2>
+                  <center><input className={styles.formInput} type="password" /><br /></center>
+                  <center><Button size="sm">Submit</Button></center>
+               </Form>
+               <Form formTitle="Register">
+                  <h2 className={styles.formDescriptor}>First Name</h2>
+                  <center><input className={styles.formInput} type="text" /><br /></center>
+                  <h2 className={styles.formDescriptor}>Last Name</h2>
+                  <center><input className={styles.formInput} type="text" /><br /></center>
+                  <h2 className={styles.formDescriptor}>Email</h2>
+                  <center><input className={styles.formInput} type="email" /><br /></center>
+                  <h2 className={styles.formDescriptor}>Password</h2>
+                  <center><input className={styles.formInput} type="password" /><br /></center>
+                  <center><Button size="sm">Register</Button></center>
+               </Form>
+               <Form formTitle="Add/Edit Event">
+                  <Button className={styles.backButton} size="sm">Back</Button>
+                  <h2 className={styles.formDescriptor}>Event Name</h2>
+                  <center><input className={styles.formInput} type="text" /><br /></center>
+                  <h2 className={styles.formDescriptor}>Event Type</h2>
+                  <center><input className={styles.formInput} type="text" /><br /></center>
+                  <h2 className={styles.formDescriptor}>Event Visibility</h2>
+                  <center><input className={styles.formInput} type="text" /><br /></center>
+                  <h2 className={styles.formDescriptor}>Description</h2>
+                  <center><input className={styles.formInput} type="text" /><br /></center>
+                  <h2 className={styles.formDescriptor}>Location</h2>
+                  <center><input className={styles.formInput} type="text" /><br /></center>
+                  <center><Button size="sm">Submit</Button></center>
+               </Form>
+               <Form formTitle="Edit RSO">
+                  <Button className={styles.backButton} size="sm">Back</Button>
+                  <h2 className={styles.formDescriptor}>Description</h2>
+                  <center><input className={styles.formInput} type="text" /><br /></center>
+                  <h2 className={styles.formDescriptor}>Number of Students</h2>
+                  <center><input className={styles.formInput} type="text" /><br /></center>
+                  <h2 className={styles.formDescriptor}>Location</h2>
+                  <center><input className={styles.formInput} type="text" /><br /></center>
+                  <center><Button size="sm">Submit</Button></center>
+               </Form>
+               <Form formTitle="Create RSO">
+                  <Button className={styles.backButton} size="sm">Back</Button>
+                  <h2 className={styles.formDescriptor}>Name</h2>
+                  <center><input className={styles.formInput} type="text" /><br /></center>
+                  <h2 className={styles.formDescriptor}>Description</h2>
+                  <center><input className={styles.formInput} type="text" /><br /></center>
+                  <h2 className={styles.formDescriptor}>Primary Owner Email</h2>
+                  <center><input className={styles.formInput} type="email" /><br /></center>
+                  <h2 className={styles.formDescriptor}>Member 1 Email</h2>
+                  <center><input className={styles.formInput} type="email" /><br /></center>
+                  <h2 className={styles.formDescriptor}>Member 2 Email</h2>
+                  <center><input className={styles.formInput} type="email" /><br /></center>
+                  <h2 className={styles.formDescriptor}>Member 3 Email</h2>
+                  <center><input className={styles.formInput} type="email" /><br /></center>
+                  <h2 className={styles.formDescriptor}>Member 4 Email</h2>
+                  <center><input className={styles.formInput} type="email" /><br /></center>
+                  <center><Button size="sm">Submit</Button></center>
+               </Form>
+               <Form formTitle="Edit University">
+                  <Button className={styles.backButton} size="sm">Back</Button>
+                  <h2 className={styles.formDescriptor}>University Name</h2>
+                  <center><input className={styles.formInput} type="text" /><br /></center>
+                  <h2 className={styles.formDescriptor}>Description</h2>
+                  <center><input className={styles.formInput} type="text" /><br /></center>
+                  <h2 className={styles.formDescriptor}>Number of Students</h2>
+                  <center><input className={styles.formInput} type="text" /><br /></center>
+                  <h2 className={styles.formDescriptor}>Location</h2>
+                  <center><input className={styles.formInput} type="text" /><br /></center>
+                  <button className={styles.uploadPictureButton}>Upload Picture</button>
+                  <br/><br/><br/><br/>
+                  <center><Button size="sm">Submit</Button></center>
+               </Form>
+            </div>
+         </div>
       </>
    );
 }
