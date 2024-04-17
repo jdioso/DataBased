90b94e1fba49be274b58c48d64991c5449523ea2
Daisy Nguyen <93001657+dhashira@users.noreply.github.com>
2024-04-17 17:50:30 -0400

    Adjusted UserEndpoints.js

--- frontend/src/utils/UserEndpoints.js
+++ frontend/src/utils/UserEndpoints.js
@@ -6,7 +6,7 @@ async function login(requestBody) {
    if (!requestBody) {
       return null;
    }
-   const url = buildPath("/api/users/register");
+   const url = buildPath("/api/users/login");
    let obj = requestBody;
 
    // settings for request
@@ -42,12 +42,15 @@ async function login(requestBody) {
          // Something happened in setting up the request that triggered an Error
          console.log("Error", error.message);
       }
-
-      // return something if there was an error
-      return { message: "Failed to Register User", userID: null };
    });
-   // return data if success
-   return response.data;
+
+   // return for failed request
+   if (!response) {
+      return null;
+   } else {
+      // return data if success
+      return response.data;
+   }
 }
 
 // function that calls login endpoint
@@ -72,7 +75,9 @@ async function register(requestBody) {
       data: obj,
    };
 
+   // handles calling request
    const response = await axios.request(config).catch((error) => {
+      // handles different error cases
       if (error.response) {
          // The request was made and the server responded with a status code
          // that falls out of the range of 2xx
@@ -88,11 +93,67 @@ async function register(requestBody) {
          // Something happened in setting up the request that triggered an Error
          console.log("Error", error.message);
       }
-      // return something if there was an error
-      return { message: "Failed to Login", userID: null };
    });
-   // return data if success
-   return response.data;
+
+   // return for failed request
+   if (!response) {
+      return null;
+   } else {
+      // return data if success
+      return response.data;
+   }
 }
 
-export { login, register };
+// function that calls login endpoint
+async function getByID(requestBody) {
+   if (id == null || !requestBody) {
+      return null;
+   }
+
+   const url = buildPath(`/api/users/user/${id}`);
+   let obj = requestBody;
+
+   let config = {
+      method: "post",
+      url: url,
+      headers: {
+         headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers":
+               "Origin, X-Requested-With, Content-Type, Accept",
+            "Content-Type": "application/json",
+         },
+      },
+      data: obj,
+   };
+
+   // handles calling request
+   const response = await axios.request(config).catch((error) => {
+      // handles different error cases
+      if (error.response) {
+         // The request was made and the server responded with a status code
+         // that falls out of the range of 2xx
+         console.log(error.response.data);
+         console.log(error.response.status);
+         console.log(error.response.headers);
+      } else if (error.request) {
+         // The request was made but no response was received
+         // `error.request` is an instance of XMLHttpRequest in the browser
+         // and an instance of http.ClientRequest in node.js
+         console.log(error.request);
+      } else {
+         // Something happened in setting up the request that triggered an Error
+         console.log("Error", error.message);
+      }
+   });
+
+   // return for failed request
+   if (!response) {
+      return null;
+   } else {
+      // return data if success
+      return response.data;
+   }
+}
+
+export { login, register, getByID};
